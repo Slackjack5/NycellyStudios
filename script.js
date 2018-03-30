d1d760089c03931e17d8b16045e7be98e6395fa6
Slackjack5 <slackjack5@gmail.com>
2018-03-30 16:35:54 -0400

    Added 3 new buttons after first button is pushed.

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 // Add event handler functions here
 
 function setup() {
-  // Setup event handlers here
+  //declaring variables
+  var roundButton = document.querySelector("#roundButton");
+  var flyoutMenu = document.querySelector("#flyoutMenu");
+  var flyoutMenu2 = document.querySelector("#flyoutMenu2");
+  var flyoutMenu3 = document.querySelector("#flyoutMenu3");
+//variables for button 1 and slideout menu
+  roundButton.addEventListener("click", showMenu, false)
+  flyoutMenu.addEventListener("click", hideMenu, false)
+  flyoutMenu2.addEventListener("click", hideMenu, false)
+  flyoutMenu3.addEventListener("click", hideMenu, false)
+//when show menu true
+  function showMenu(e) {
+      flyoutMenu.classList.add("show");
+      flyoutMenu2.classList.add("show");
+      flyoutMenu3.classList.add("show");
+      document.body.style.overflow = "hidden";
+  }
+
+//when hide menu true
+  function hideMenu(e) {
+      flyoutMenu.classList.remove("show");
+      flyoutMenu2.classList.remove("show");
+      flyoutMenu3.classList.remove("show");
+      e.stopPropagation();
+
+      document.body.style.overflow = "auto";
+  }
 
 }
 
